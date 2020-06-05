401034f4153254a19e002e7936308dce423b25a3
fredcode <fredcode23@gmail.com>
2020-06-05 13:08:59 +0800

    mock home page created

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, redirect, render_template, request, flash, url_for
 from app.models import User
 from app import db
 from flask_wtf import FlaskForm
+from flask_login import login_required
 #from flask_wtf.file import FileField, FileAllowed
 #from flask_uploads import IMAGES
 #import random
@@ -10,5 +11,10 @@ main = Blueprint('main', __name__)
 
 
 @main.route("/")
+def index():
+  return(render_template("layout.html"))
+
+@main.route("/home")
+@login_required
 def home():
-  pass
+  return(render_template("home.html"))
